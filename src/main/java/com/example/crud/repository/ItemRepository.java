09da86af753518e1e65b6d889af9f9780b17620d
Dev-Harshxl <167305946+Dev-Harshxl@users.noreply.github.com>
2025-01-27 09:16:20 +0530

    Delete src directory

--- src/main/java/com/example/crud/repository/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.crud.repository;
-
-import com.example.crud.entity.items;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<items, Long> {
-}
-// Repository
